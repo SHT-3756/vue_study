af7f91a51b960bdb06a0ae0cbd4df7a656c78032
SHT-3756 <gkdlfndkekd@naver.com>
2023-06-09 00:47:01 +0900

    feat : 프로젝트 생성

---
+++ project_vue_1/frontend/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Ground from "./components/Ground.vue";
+</script>
+
+<template>
+  <div class="app">
+    <Ground />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ project_vue_1/frontend/src/components/Ground.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="ground">
+    <Header />
+    <div>I am Ground</div>
+    <Footer />
+  </div>
+</template>
+
+<script setup>
+import Header from "./Header.vue";
+import Footer from "./Footer.vue";
+</script>
